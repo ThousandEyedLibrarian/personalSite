c47e778a5efb390bbf9ace82259eb574ae1ffc2e
CarterFaceySmith <carterfaceysmith@gmail.com>
2023-08-21 15:55:30 +0200

    Added multilingual welcome

--- src/scripts.js
+++ src/scripts.js
@@ -8,3 +8,24 @@ window.addEventListener('scroll', function () {
     titleContainer.style.opacity = Math.max(0, 1 - 2 * progress);
     scrollTxtContainer.style.opacity = Math.max(0, 1 - 4 * progress);
 });
+
+(function() {
+    const container = document.getElementById("title-container");
+    const elements = container.querySelectorAll('i');
+    let currentIndex = 0;
+
+    setInterval(() => {
+        // Hide the current visible element
+        elements[currentIndex].style.opacity = "0";
+
+        // Move to the next element
+        currentIndex = (currentIndex + 1) % elements.length;
+
+        // Delay the showing of the next element to sync with the fade out effect
+        setTimeout(() => {
+            elements[currentIndex].style.opacity = "1";
+        }, 1000); // Matches the 1s transition duration in the CSS
+
+    }, 5000); // 5000 milliseconds or 5 seconds
+})();
+
